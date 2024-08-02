7bcfb1385f8822795f784d6099174bf8d53bc247
naodmulu <naodmulugeta1621@gmail.com>
2024-08-02 10:04:06 +0300

    Task 5 initail stages commit

---
+++ Task 5/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className="container">
+      {/* tab for Name of form */}
+      <div className="tab">
+        <h1 className="title">
+          Contact Us
+        </h1>
+      </div>
+
+
+      {/* container for the form */}
+      <div className="formContainer">
+        Form
+      </div>
+    </div>
+
+    )
+}
+
+export default App
